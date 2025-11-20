f04429a6abd170030404ff9fc2682af33651ab5c
skriku-lab <riku.riku.rikuto0606@gmail.com>
2025-11-20 15:05:47 +0900

    20251120 v7

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -3,13 +3,13 @@
 //1.在庫データ（JSON的な構造）
 //--------------------
 const items = [
-    {id: "D001", name:"オレンジジュース",price: 150,stock: 20,minStock: 5},
-    {id: "D002", name:"コーラ",         price: 150,stock: 12,minStock: 5},
-    {id: "D001", name:"オレンジジュース",price: 120,stock: 30,minStock: 10},
+    { id: "D001", name:"オレンジジュース",price: 150,stock: 20,minStock: 5},
+    { id: "D002", name:"コーラ",         price: 150,stock: 12,minStock: 5},
+    { id: "D003", name:"お茶",           price: 120,stock: 30,minStock: 10},
 ];
 
 //HTML で <tbody id="item-list"> と書いた部分を JavaScript から取得する
-const tbody = document.getElementByld("item-list");
+const tbody = document.getElementById("item-list");
 
 //-------------------
 //2.表示用の関数 render()
@@ -24,15 +24,15 @@ function render() {
 
         // 在庫が少ないときはクラスを追加して背景色を変える
         if(item.stock < item.minStock) {
-            tr.classList.add("low-stock");
+          tr.classList.add("low-stock");
         }
 
         // 行の中身（セル）をテンプレート文字列でまとめて書く
         tr.innerHTML = `
-           <td>${item.id}<t/d>
-           <td>${item.name}<t/d>
-           <td>${item.price}<t/d>
-           <td>${item.stock}<t/d>
+           <td>${item.id}</td>
+           <td>${item.name}</td>
+           <td>${item.price}</td>
+           <td>${item.stock}</td>
            <td>
              <button onclick="changeStock('${item.id}',1)">＋</button>
              <button onclick="changeStock('${item.id}',-1)">ー</button>
@@ -58,7 +58,7 @@ function changeStock(id, diff) {
     // diff 分だけ在庫数を増減させる
     item.stock += diff;
     // 在庫がマイナスにならないように０で止める
-    if(item.stock <0 )  {
+    if(item.stock < 0)  {
       item.stock = 0;
     }
 
