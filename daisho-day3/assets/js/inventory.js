e26e098bb14f1da563002e8dca2bc7fdba7ef2a7
skriku-lab <riku.riku.rikuto0606@gmail.com>
2025-11-20 14:54:53 +0900

    20251120 v6

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -1,26 +1,3 @@
-const items = [
-    {
-        id: "D001",
-        name: "オレンジジュース",
-        price: 150,
-        stock: 20,
-        minStock: 5
-    },
-    {
-        id: "D002",
-        name: "コーラ",
-        price: 150,
-        stock: 12,
-        minStock: 5
-    },
-    {
-        id: "D003",
-        name: "お茶",
-        price: 120,
-        stock: 30,
-        minStock: 10
-    }
-];
 
 //--------------------
 //1.在庫データ（JSON的な構造）
