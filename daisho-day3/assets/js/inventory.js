b9f035ee0aecc90043de7b69176d11ffa36d4c13
skriku-lab <riku.riku.rikuto0606@gmail.com>
2025-11-20 14:19:51 +0900

    20251120 v2

---
+++ daisho-day3/assets/js/inventory.js
@@ -0,0 +1,23 @@
+const items = [
+    {
+        id: "D001",
+        name: "オレンジジュース",
+        price: 150,
+        stock: 20,
+        minStock: 5
+    },
+    {
+        id: "D002",
+        name: "コーラ",
+        price: 150,
+        stock: 12,
+        minStock: 5
+    },
+    {
+        id: "D003",
+        name: "お茶",
+        price: 120,
+        stock: 30,
+        minStock: 10
+    }
+];
